47568597a8d4dea9a587b915631dd84931283ead
SALL Oumar <119567549+oumar146@users.noreply.github.com>
2024-05-24 11:24:58 +0200

    add router
    add 3 routes : 
    - home page
    - rules page 
    -  game page

--- tmdb-app/src/App.js
+++ tmdb-app/src/App.js
@@ -1,7 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import RulesPage from './pages/RulesPage';
+import GamePage from './pages/GamePage';
 import './App.css';
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="sidebar">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/rules" element={<RulesPage />} />
+          <Route path="/game" element={<GamePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
