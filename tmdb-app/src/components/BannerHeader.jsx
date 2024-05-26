6ea6d6aeabf21128f01171016234682f074e7fc6
SALL Oumar <119567549+oumar146@users.noreply.github.com>
2024-05-27 01:22:36 +0200

    correction navigation

--- tmdb-app/src/components/BannerHeader.jsx
+++ tmdb-app/src/components/BannerHeader.jsx
@@ -8,14 +8,15 @@ function BannerHeader() {
             <h1>A quel point connaissez-vous le septième art ?</h1>
             <p>Découvrez-le en jouant à nos divers jeux</p>
             <div class="cta">
-                <a href="rules.html">
-                    <button class="btn rules"><NavLink to='/rules'>Comment Jouer ?</NavLink></button>
-                </a>
+                <NavLink to='/rules'>
+                    <button class="btn rules">Comment Jouer ?
+                    </button>
+                </NavLink>
                 <a href="#game-section">
                     <button class="btn launch">Lancer une partie</button>
                 </a>
             </div>
-        </div>
+        </div >
     );
 }
 
